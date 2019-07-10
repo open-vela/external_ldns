aaba99528055174e53a91c5f06bfd56aa10242b8
openvela-robot <openvela-robot@xiaomi.com>
2019-07-10 13:25:11 +0800

    Merge branch 'develop' of github.com:NLnetLabs/ldns into develop

--- keys.c
+++ keys.c
@@ -17,11 +17,6 @@
 #ifdef HAVE_SSL
 #include <openssl/ssl.h>
 #include <openssl/rand.h>
-#include <openssl/bn.h>
-#include <openssl/rsa.h>
-#ifdef USE_DSA
-#include <openssl/dsa.h>
-#endif
 #ifndef OPENSSL_NO_ENGINE
 #include <openssl/engine.h>
 #endif
--- test/32-unbound-regression.tpkg/32-unbound-regression.clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -fr `ls -1a|grep -v ^32-|grep -v '^\.$'|grep -v '^\.\.$'`
-
