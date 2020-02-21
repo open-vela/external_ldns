57ddf5587883ccfaf5f4dda5adc09c97d5b8d2f7
liujinye <liujinye@xiaomi.com>
2020-02-21 11:14:53 +0800

    Merge pull request #69 from neheb/patch-1
    Fix cross compilation under OS X

--- rr_functions.c
+++ rr_functions.c
@@ -269,17 +269,14 @@ ldns_rr_dnskey_key_size_raw(const unsigned char* keydata,
                             const size_t len,
                             const ldns_algorithm alg)
 {
-#ifdef USE_DSA
 	/* for DSA keys */
 	uint8_t t;
-#endif /* USE_DSA */
 	
 	/* for RSA keys */
 	uint16_t exp;
 	uint16_t int16;
 	
 	switch ((ldns_signing_algorithm)alg) {
-#ifdef USE_DSA
 	case LDNS_SIGN_DSA:
 	case LDNS_SIGN_DSA_NSEC3:
 		if (len > 0) {
@@ -289,7 +286,6 @@ ldns_rr_dnskey_key_size_raw(const unsigned char* keydata,
 			return 0;
 		}
 		break;
-#endif /* USE_DSA */
 	case LDNS_SIGN_RSAMD5:
 	case LDNS_SIGN_RSASHA1:
 	case LDNS_SIGN_RSASHA1_NSEC3:
