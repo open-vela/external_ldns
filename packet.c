ca61eecec651a5d485eb0432938701565c1878c2
liujinye <liujinye@xiaomi.com>
2022-04-14 15:32:24 +0800

    Merge pull request #171 from dkg/fix-destclean-target
    Revert "spelling: distclean"

--- packet.c
+++ packet.c
@@ -262,12 +262,6 @@ ldns_pkt_edns_data(const ldns_pkt *packet)
 	return packet->_edns_data;
 }
 
-ldns_edns_option_list*
-ldns_pkt_edns_option_list(const ldns_pkt *packet)
-{
-	return packet->_edns_list;
-}
-
 /* return only those rr that share the ownername */
 ldns_rr_list *
 ldns_pkt_rr_list_by_name(const ldns_pkt *packet,
@@ -645,13 +639,6 @@ ldns_pkt_set_edns_data(ldns_pkt *packet, ldns_rdf *data)
 }
 
 void
-ldns_pkt_set_edns_option_list(ldns_pkt *packet, ldns_edns_option_list *list)
-{
-	packet->_edns_list = list;
-}
-
-
-void
 ldns_pkt_set_section_count(ldns_pkt *packet, ldns_pkt_section s, uint16_t count)
 {
 	switch(s) {
@@ -751,101 +738,13 @@ ldns_pkt_safe_push_rr_list(ldns_pkt *p, ldns_pkt_section s, ldns_rr_list *list)
 }
 
 bool
-ldns_pkt_edns(const ldns_pkt *pkt)
-{
+ldns_pkt_edns(const ldns_pkt *pkt) {
 	return (ldns_pkt_edns_udp_size(pkt) > 0 ||
 		ldns_pkt_edns_extended_rcode(pkt) > 0 ||
 		ldns_pkt_edns_data(pkt) ||
 		ldns_pkt_edns_do(pkt) ||
                 pkt->_edns_present
 	       );
-}
-
-ldns_edns_option_list*
-ldns_pkt_edns_get_option_list(const ldns_pkt *packet)
-{
-	size_t pos = 0;
-	ldns_edns_option_list* edns_list;
-	size_t max;
-	const uint8_t* wire;
-
-	if (!ldns_pkt_edns_data(packet))
-		return NULL;
-
-	/* return the list if it already exists */
-	if (packet->_edns_list != NULL) {
-		return packet->_edns_list;
-	}
-
-	/* if the list doesn't exists, we create it by parsing the _edns_data */
-
-	max = ldns_rdf_size(ldns_pkt_edns_data(packet));
-	wire = ldns_rdf_data(ldns_pkt_edns_data(packet));
-	// @TODO do checks so we don't read into un-auth memory (max !< 4)
-
-	edns_list = ldns_edns_option_list_new();
-	if (edns_list == NULL) {
-		return NULL;
-	}
-
-	while (pos < max) {
-		ldns_edns_option* edns;
-		uint8_t *data;
-		if (pos + 4 > max) {
-			ldns_edns_option_list_deep_free(edns_list);
-			return NULL;
-		}
-		ldns_edns_option_code code = ldns_read_uint16(&wire[pos]);
-		size_t size = ldns_read_uint16(&wire[pos+2]);
-		pos += 4;
-		if (pos + size > max) {
-			ldns_edns_option_list_deep_free(edns_list);
-			return NULL;
-		}
-		data = LDNS_XMALLOC(uint8_t, size);
-
-		if (!data) {
-			ldns_edns_option_list_deep_free(edns_list);
-			return NULL;
-		}
-		memcpy(data, &wire[pos], size);
-		pos += size;
-
-		edns = ldns_edns_new(code, size, data);
-
-		if (!edns) {
-			ldns_edns_option_list_deep_free(edns_list);
-			return NULL;
-		}
-		ldns_edns_option_list_push(edns_list, edns);
-
-	}
-
-	return edns_list;
-}
-
-bool
-ldns_pkt_edns_write_option_list_to_edns_data(ldns_pkt *packet,
-	ldns_edns_option_list* edns_list)
-{
-	ldns_buffer* buffer;
-	ldns_rdf* edns_rdf;
-
-	/* get the EDNS options in wireformat */
-	buffer = ldns_edns_option_list2wireformat_buffer(edns_list);
-
-	if (buffer == NULL) {
-		return false;
-	}
-
-	/* read the data and create the EDNS RDF */
-	edns_rdf = ldns_rdf_new(LDNS_RDF_TYPE_UNKNOWN, ldns_buffer_limit(buffer),
-		ldns_buffer_export(buffer));
-
-	/* set the new EDNS RDF in the packet */
-	ldns_pkt_set_edns_data(packet, edns_rdf);
-
-	return true;
 }
 
 
@@ -897,9 +796,8 @@ ldns_pkt_new(void)
 	ldns_pkt_set_edns_version(packet, 0);
 	ldns_pkt_set_edns_z(packet, 0);
 	ldns_pkt_set_edns_data(packet, NULL);
-	ldns_pkt_set_edns_option_list(packet, NULL);
 	packet->_edns_present = false;
-
+	
 	ldns_pkt_set_tsig(packet, NULL);
 	
 	return packet;
@@ -916,7 +814,6 @@ ldns_pkt_free(ldns_pkt *packet)
 		ldns_rr_list_deep_free(packet->_additional);
 		ldns_rr_free(packet->_tsig_rr);
 		ldns_rdf_deep_free(packet->_edns_data);
-		ldns_edns_option_list_deep_free(packet->_edns_list);
 		ldns_rdf_deep_free(packet->_answerfrom);
 		LDNS_FREE(packet);
 	}
@@ -1266,9 +1163,6 @@ ldns_pkt_clone(const ldns_pkt *pkt)
 		ldns_pkt_set_edns_data(new_pkt, 
 			ldns_rdf_clone(ldns_pkt_edns_data(pkt)));
 	ldns_pkt_set_edns_do(new_pkt, ldns_pkt_edns_do(pkt));
-	if (ldns_pkt_edns_option_list(pkt))
-		ldns_pkt_set_edns_option_list(new_pkt,
-			ldns_edns_option_list_clone(ldns_pkt_edns_get_option_list(pkt)));
 
 	ldns_rr_list_deep_free(new_pkt->_question);
 	ldns_rr_list_deep_free(new_pkt->_answer);
